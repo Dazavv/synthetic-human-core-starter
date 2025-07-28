abf521144d7227f1a6480bbaba4df5e05a034e5d
dazavv <dasha7348@gmail.com>
2025-07-28 21:26:01 +0300

    updated

--- src/main/java/org/example/starter/audit/service/console/ConsoleAudit.java
+++ src/main/java/org/example/starter/audit/service/console/ConsoleAudit.java
@@ -1,11 +1,9 @@
 package org.example.starter.audit.service.console;
 
-import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
 import org.example.starter.audit.model.AuditLog;
 import org.example.starter.audit.service.AuditLogger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -15,6 +13,6 @@ public class ConsoleAudit implements AuditLogger {
 
     @Override
     public void log(AuditLog auditLog) {
-        log.info("AUDIT_LOG: {}", new Gson().toJson(auditLog));
+        log.info("AUDIT_LOG: {}", auditLog);
     }
 }
--- src/main/java/org/example/starter/command/service/executors/CommandQueueService.java
+++ src/main/java/org/example/starter/command/service/executors/CommandQueueService.java
@@ -4,26 +4,29 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.starter.command.model.Command;
 import org.example.starter.command.model.CommandPriority;
 import org.example.starter.command.service.exceptions.QueueOverflowException;
-import org.example.starter.command.service.validation.CommandValidator;
+import org.example.starter.command.CommandValidator;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.*;
 
 @Slf4j
+@Service
 public class CommandQueueService {
 
     private final BlockingQueue<Runnable> commandsQueue;
     private final ThreadPoolExecutor executor;
     private final CommandValidator validator;
     private final CommandExecutor commandExecutor;
+    private final int CORE_POOL_SIZE = 2;
+    private final int MAX_POOL_SIZE = 4;
+    private final int QUEUE_CAPACITY = 100;
 
-    public CommandQueueService(int corePoolSize,
-                               int maxPoolSize,
-                               int queueCapacity,
-                               CommandValidator validator) {
-        this.commandsQueue = new ArrayBlockingQueue<>(queueCapacity);
+
+    public CommandQueueService(CommandValidator validator) {
+        this.commandsQueue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
         this.executor = new ThreadPoolExecutor(
-                corePoolSize,
-                maxPoolSize,
+                CORE_POOL_SIZE,
+                MAX_POOL_SIZE,
                 60L,
                 TimeUnit.SECONDS,
                 commandsQueue,
